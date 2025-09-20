89c6d27fc0c36a5e6f9e38711df00b0574fb147d
emmanuel1611 <muhindoemma39@gmail.com>
2025-09-20 18:59:02 +0300

    Comment out dark mode toggle button in Navbar component

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -230,18 +230,7 @@ const Navbar = () => {
             )}
           </div>
 
-          {/* Dark mode toggle */}
-          {/* <button
-            onClick={toggleDarkMode}
-            className="p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-transform hover:scale-105"
-            aria-label="Toggle dark mode"
-          >
-            {isDarkMode ? (
-              <Sun className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-            ) : (
-              <Moon className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-            )}
-          </button> */}
+
 
           {/* Notifications */}
           <div className="relative" ref={notifRef}>
